c6067e6888c22a1e09312d60427d77a866a1009d
Jay Brass <jay.brass@gmail.com>
2026-06-29 13:37:00 -0400

    actionable elements

--- src/core/getActionableElements.ts
+++ src/core/getActionableElements.ts
@@ -1,6 +1,10 @@
 import type { Page, ElementHandle } from "playwright";
 
-export async function getActionableElements(page: Page): Promise<ElementHandle[]> {
-	const elements = await page.$$("a[href], button, input, select, textarea, details, [role='button'], [role='link'], [role='checkbox'],[role='menuitem'], [role='option'], [role='radio'], [role='switch'], [role='textbox'], [tabindex]:not([tabindex='-1']), [contenteditable='true']")
-	return elements;
+export async function getActionableElements(
+  page: Page,
+): Promise<ElementHandle[]> {
+  const elements = await page.$$(
+    "a[href], button, input, select, textarea, [role='button'], [role='link'], [role='checkbox'],[role='menuitem'], [role='option'], [role='radio'], [role='switch'], [role='textbox'], [tabindex]:not([tabindex='-1']), [contenteditable='true']",
+  );
+  return elements;
 }
